093fc81b807704bdcced38f61773f6ac33d15216
Dhemoz <87259819+dhemoz@users.noreply.github.com>
2023-01-29 15:04:48 +0700

    Delete terraform.tf

--- terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform { 
-    required_version = "~> 1.3.7"
-    required_providers {
-        aws = {
-            version = "~> 4.24.0"
-//Version aws provider yang disupport
-        }
-    }
-}
